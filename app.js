1c91c347447727f2aa27e102519692d2a23fe930
jbrooks036 <joannbrooks036@gmail.com>
2015-05-05 22:52:34 -0500

    fizz buzz html + js

---
+++ app.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>A Web Page</h1>
+
+<script>
+for (i=1; i<100; i++)
+{
+  if (i % 15 == 0) console.log("FizzBuzz");
+  else if (i % 3 == 0) console.log("Fizz");
+  else if (i % 5 == 0) console.log("Buzz");
+  else console.log(i);
+}
+</script>
+
+</body>
+</html> 
+
